cea7750997cc6c2aebfe488cd6dac1f885d0cee5
rehemae <rehemaephraimnanda@gmail.com>
2022-08-10 18:19:10 +0300

    first commit

---
+++ assesment.js
@@ -0,0 +1,28 @@
+
+
+
+var name= function() {  
+    alert('Hello ' + names);
+}
+function username(callback) {
+        var name = putname(' enter your name.');
+        callback(name);
+      }
+ 
+username (names);
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
